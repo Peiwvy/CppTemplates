3829b4788e8d465b0157e79dbe473a4ec3d1b780
ztyu <peiwvy@163.com>
2023-01-04 07:43:44 +0000

    ⚡️ dockerfile changes

--- .devcontainer/toolchains.sh
+++ .devcontainer/toolchains.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-sudo apt update
-
-# install GCC9
-sudo apt install -y software-properties-common
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt update
-sudo apt install -y gcc-9 g++-9 build-essential
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 9
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 9
-
 # install latest cmake
-# sudo bash -c "$(wget -O - https://apt.kitware.com/kitware-archive.sh)"
-# sudo apt install -y cmake cmake-qt-gui
+sudo bash -c "$(wget -O - https://apt.kitware.com/kitware-archive.sh)"
+sudo apt install -y cmake cmake-qt-gui
 
 # install latest clangd
 sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-15 100
 
-#install latest doxygen
-sudo apt install -y doxygen
+# 清华源下载 llvm (optional)
+# 下载脚本 from https://mirror.tuna.tsinghua.edu.cn/help/llvm-apt/
+# wget https://mirrors.tuna.tsinghua.edu.cn/llvm-apt/llvm.sh
+# chmod +x llvm.sh
+# sudo ./llvm.sh all -m https://mirrors.tuna.tsinghua.edu.cn/llvm-apt
+# sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-15 100
